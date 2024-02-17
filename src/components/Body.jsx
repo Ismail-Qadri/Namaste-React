fd35e10803ffa44d46255c9343e62293b22481ef
Mohammad Ismail Qadri <ismailqadri98@gmail.com>
2024-02-17 18:28:01 +0530

    complete code

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -9,14 +9,12 @@ const Body=()=>{
 
     useEffect(()=>{
       // console.log("useEffect called")
-      fetchData();
+      fetchDatA();
     }, []);
     
-    const fetchData= async()=>{
+    const fetchDatA= async()=>{
       const data= await fetch(
-        'https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING',{
-          mode: 'no-cors'
-        }
+        'https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING',
       );
       const json=await data.json();
       console.log(json);
